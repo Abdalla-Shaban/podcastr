357a6c07f4b7ed7e1a6c9a1ee0e697147e3542f1
abdalla <abdallashaban584@gmail.com>
2024-06-14 15:20:44 +0300

    add constants file

---
+++ api/utils/constants.js
@@ -0,0 +1,8 @@
+export const PORT = process.env.PORT || 5000;
+export const MONGODB_URL = process.env.MONGODB_URL;
+
+export const statusMessage = {
+  SUCCESS: "success",
+  FAIL: "fail",
+  ERROR: "error",
+};
